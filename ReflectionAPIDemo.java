d9c1d6ea697e3f8e4c9305ebd42b86b570894cec
Ankur Yadav <36620764+yankur2110@users.noreply.github.com>
2020-07-07 10:16:49 +0530

    Add files via upload

---
+++ ReflectionAPIDemo.java
@@ -0,0 +1,28 @@
+package java_practice;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+class Test{
+	@SuppressWarnings("unused")
+	private void testMethod(){
+		System.out.println("I am inside private test method.");
+	}
+}
+
+public class ReflectionAPIDemo {
+	
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException{
+		
+		Class<?> classObject = Class.forName("java_practice.Test");
+		Test testObjectUsingClass = (Test)classObject.newInstance();
+		
+		Method m = classObject.getDeclaredMethod("testMethod");
+		m.setAccessible(true);
+		m.invoke(testObjectUsingClass);
+		
+		
+	}
+
+
+}
