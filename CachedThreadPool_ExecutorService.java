9d76cec5f4b77565cf31470e04d8435e24cb5f7b
Ankur Yadav <36620764+yankur2110@users.noreply.github.com>
2020-07-12 21:47:57 +0530

    Add files via upload

---
+++ CachedThreadPool_ExecutorService.java
@@ -0,0 +1,24 @@
+package java_practice;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class TaskForCachedPool implements Runnable{
+	@Override
+	public void run() {
+		System.out.println("ThreadName is "+Thread.currentThread().getName());
+	}
+}
+
+public class CachedThreadPool_ExecutorService {
+
+	public static void main(String[] args){
+		//here we don't specify the number of threads to be created as parameter.
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		for(int i=0;i<1000;i++){
+			executorService.execute(new TaskForCachedPool());
+		}
+		System.out.println("I am main thread"+ Thread.currentThread().getName());
+		
+	}
+}
