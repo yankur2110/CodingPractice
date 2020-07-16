73524ab689d7fb59502fc907761090ede477235b
Ankur Yadav <36620764+yankur2110@users.noreply.github.com>
2020-07-16 23:53:45 +0530

    Add files via upload

---
+++ CallableDemo.java
@@ -0,0 +1,34 @@
+package java_practice;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+class DemoTask implements Callable<Integer>{
+
+	@Override
+	public Integer call() throws Exception {
+		Thread.sleep(3000);
+		return new Random().nextInt();
+	}
+	
+}
+	
+public class CallableDemo {
+	
+	public static void main(String[] args) throws InterruptedException, ExecutionException{
+		ExecutorService executorService  = Executors.newFixedThreadPool(10);
+		
+		//submit the task for execution
+		Future<Integer> future = executorService.submit(new DemoTask());
+		
+		//store the value when returned.
+		Integer result = future.get();
+		
+		System.out.println("returned value from callable is "+result);
+	}
+
+}
